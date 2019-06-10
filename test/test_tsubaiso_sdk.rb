7060792d6c3e8d693043ca4f144fb9185984b7aa
pyons04 <broad.basic15@gmail.com>
2019-06-10 16:46:29 +0900

    fix conflictions

--- test/test_tsubaiso_sdk.rb
+++ test/test_tsubaiso_sdk.rb
@@ -246,71 +246,12 @@ class TsubaisoSDKTest < Minitest::Test
       sort_number: '0'
     }
 
-    @bank_account_master_1 = {
-      name: "ANZ Bank",
-      account_type: "1",
-      account_number: "66667777",
-      nominee: "tsubaiso taro",
-      memo: "",
-      start_ymd: "2019-06-01",
-      finish_ymd: nil,
-      zengin_bank_code: "7777",
-      zengin_branch_code: "777",
-      zengin_client_code_sogo: "9999999999",
-      zengin_client_code_kyuyo: nil,
-      currency_code: "",
-      currency_rate_master_id: nil
-    }
-  end
-
-  def test_create_bank_account_master
-    created_bank_account_master = @api.create_bank_account_master(@bank_account_master_1)
-  ensure
-    result = @api.destroy_bank_account_master(created_bank_account_master[:json][:id]) if created_bank_account_master[:json][:id]
-    puts result.inspect
-  end
-
-  def test_list_bank_account_master
-    list = @api.list_bank_account_master
-    puts list.inspect
-    #assert_equal 200, index[:status].to_i, index.inspect
-  end
-
-  def test_update_bank_account_master
-    created_bank_account_master = @api.create_bank_account_master(@bank_account_master_1)
-    update_attributes = {
-      name = "NationalAustraliaBank"
-    }
-
-  ensure
-    result = @api.destroy_bank_account_master(created_bank_account_master[:json][:id]) if created_bank_account_master[:json][:id]
-    puts result.inspect
-  end
-
-
   def test_failed_request
     @api_fail = TsubaisoSDK.new({ base_url: ENV['SDK_BASE_URL'], access_token: 'fake token' })
     sale = @api_fail.create_sale(@sale_201608)
 
     assert_equal 401, sale[:status].to_i, sale.inspect
     assert_equal 'Bad credentials', sale[:json][:error]
-  end
-
-  def test_index_api_history
-    index = @api.index_api_history
-    assert_equal 200, index[:status].to_i, index.inspect
-    assert !index[:json].empty?
-
-    initial_api_count = if index[:json].first[:ym] == "#{Time.now.year}#{format('%02d', Time.new.month)}"
-                          index[:json].first[:cnt]
-                        else
-                          0
-                        end
-
-    index = @api.index_api_history
-    assert_equal 200, index[:status].to_i, index.inspect
-    new_api_count = index[:json].first[:cnt]
-    assert_equal initial_api_count + 1, new_api_count
   end
 
   def test_create_physical_inventory_masters
